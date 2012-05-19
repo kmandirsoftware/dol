9d569d18873db33fb150d1624b43b4390f04b408
kmandirsoftware <kgerhads@comcast.net>
2012-05-19 16:35:00 -0600

    first commit which has scrolling images

---
+++ js/peel.js
@@ -0,0 +1,13 @@
+    $(function() {
+      $('body').peelback({
+        adImage  : 'peel-ad.png',
+        peelImage  : 'assets/peel-image.png',
+        clickURL : 'http://www.thebestdinosaur.com/',
+        smallSize: 50,
+        bigSize: 500,
+        gaTrack  : true,
+        gaLabel  : '#1 Stegosaurus',
+        autoAnimate: true
+      });
+    });
+
